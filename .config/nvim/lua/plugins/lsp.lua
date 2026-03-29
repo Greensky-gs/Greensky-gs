5c84dc332a8f81a61be297260883a26870630885
Greensky-gs <david.hesliere@gmail.com>
2026-03-29 12:16:14 +0200

    Add python detection

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,23 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		vim.lsp.config("clangd", {})
+		vim.lsp.config("clangd", {
+			root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", "makefile" },
+		})
 		vim.lsp.config("pyright", {
 			root_markers = { "requirements.txt", "main.py", "pyproject.toml", "pyrightconfig.json" },
+			servers = {
+				pyright = {
+					settings = {
+						python = {
+							analysis = {
+								autoSearchPaths = true,
+								diagnosticMode = "openFilesOnly",
+							},
+						}
+					},
+				},
+			},
 		})
 
 		vim.diagnostic.config({ virtual_text = true })
