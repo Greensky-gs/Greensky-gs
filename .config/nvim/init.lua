ef37a6773bb2a92b3e7430bb33fabfd5aa06c74e
Greensky-gs <david.hesliere@gmail.com>
2026-04-11 22:42:49 +0200

    Clean

--- .config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require('config.options')
-require('config.keybinds')
-require('config.lazy')
-
-vim.lsp.enable("clangd")
-vim.lsp.enable("pyright")
-
--- Installation : npm i -g pyright pour le serveur LSP pyright
---              : install clangd (normalement avec clang/gcc)
